742fc8cf951bfe00ea1808a8a99c62d615294259
AbhiDevepl <developerabhi596@gmail.com>
2025-12-23 20:02:33 +0530

    Add all project files

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+function App() {
+
+  useGSAP(()=>{
+    const tl = gsap.timeline();
+    
+    tl.to("vi-mask-group",{
+      rotate: 10,
+      duration:2,
+      ease: "Power4.easeInOut",
+      transform: "50% 50%",
+
+
+    })
+    .to(".vi-mask-group",{
+      scale: 10,
+      duration: 2,
+      delay: -1.8,
+      ease : "Expo.easeInOut",
+      transformOrigin: "50% 50%",
+      opacity: 0
+    }); 
+  });
+
+  return (
+    <>
+      <div className="svg fix top-0 left z-[100] w-full h-screen overflow-hidden bg-[#000]">
+        <svg viewBox='0 0 800 600' preserveAspectRatio='xMinYMid slice'>
+          <defs>
+            <mask id='viMask'>
+              <rect width="100%" height="100%" fill='black' />
+              <g className='vi-mask-group'>
+                <text
+                x="50%"
+                y="50%"
+                fontSize={250}
+                textAnchor='middle'
+                fill='white'
+                dominantBaseline='middle'
+                fontFamily='Arial Black'>
+                  VI
+                </text>
+              </g>
+            </mask>
+          </defs>
+        </svg>
+      </div>
+    </>
+  );
+}
+
+export default App;
